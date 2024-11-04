3846b98716a1ae594691bfade9a7b57cb8403695
Lukas Snoek <lukassnoek@gmail.com>
2024-11-04 15:59:10 +0100

    Simplify analysis

--- fpr_experiment/params.py
+++ fpr_experiment/params.py
@@ -2,9 +2,9 @@ from itertools import product
 
 ITERS = 500
 
-N = [50, 100, 500, 1000]
-K = [1, 5, 10, 50, 100]
-target_col = ['y', 'y_pred', 'fp', 'fn', 'err', 'fp_prec', 'fn_rec']
+N = [50, 500, 1000]
+K = [1, 5, 50, 100]
+target_col = ['y_pred', 'fp', 'fn', 'err']#, 'fp_prec', 'fn_rec']
 bonf_correct = [True, False]
-method = ['hbac', 'randomclusters']#, 'kmeans', 'kmeans_cv', 'randomclusters']
+method = ['hbac', 'randomclusters']
 params = list(product(method, N, K, target_col, bonf_correct))
--- fpr_experiment/run_experiment.py
+++ fpr_experiment/run_experiment.py
@@ -1,5 +1,3 @@
-from copy import copy
-from itertools import product
 from collections import defaultdict
 from pathlib import Path
 import warnings
@@ -12,70 +10,52 @@ from scipy.stats import ttest_ind
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from unsupervised_bias_detection.clustering import BiasAwareHierarchicalKMeans
+from unsupervised_bias_detection.clustering import BiasAwareHierarchicalKMeansV2
+from unsupervised_bias_detection.clustering._bahc_v2 import BiasAwareHierarchicalClusteringV2
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 
 
+METRICS = {
+    'y': lambda y, y_pred: y,
+    'y_pred': lambda y, y_pred: y_pred,
+    'fp': lambda y, y_pred: np.logical_and(y == 0, y_pred == 1).astype(int),
+    'fn': lambda y, y_pred: np.logical_and(y == 1, y_pred == 0).astype(int),
+    'err': lambda y, y_pred: (y != y_pred).astype(int),
+    'fp_prec': lambda y, y_pred: (y[y_pred == 1] != y_pred[y_pred == 1]).astype(int),
+    'fn_rec': lambda y, y_pred: (y[y == 1] != y_pred[y == 1]).astype(int)
+}
+
 def simulate_null_data(method, N, K, target_col):
 
-    # This ensures a perfectly balanced binary target 
-    y = np.random.permutation([0] * N + [1] * N)
-    model = make_pipeline(StandardScaler(), LogisticRegression())
-
     # Simulate random design matrix `X`
-    X = np.random.randn(N*2, K)
-
-    # Split in train and test set
+    X = np.random.randn(N * 2, K)
+    y = np.random.choice([0, 1], size=N * 2)
     X_train, X, y_train, y = train_test_split(X, y, test_size=0.5, stratify=y)
 
-    # Obtain cross-validated predictions
+    model = make_pipeline(StandardScaler(), LogisticRegression())
+
+    # Get cross-validated predictions
     model.fit(X_train, y_train)
     y_pred = model.predict(X)
+    target = METRICS[target_col](y, y_pred)
 
-    # Define target
-    if target_col == 'y':
-        target = y
-    elif target_col == 'y_pred':
-        target = y_pred    
-    elif target_col == 'fp':
-        target = np.logical_and(y == 0, y_pred == 1).astype(int)
-    elif target_col == 'fn':
-        target = np.logical_and(y == 1, y_pred == 0).astype(int)
-    elif target_col == 'err':
-        target = (y != y_pred).astype(int)
-    elif target_col == 'fp_prec':
-        target = (y[y_pred == 1] != y_pred[y_pred == 1]).astype(int)
-        X = X[y_pred == 1, :]
-    elif target_col == 'fn_rec':
-        target = (y[y == 1] != y_pred[y == 1]).astype(int)
-        X = X[y == 1, :]
-    else:
-        raise ValueError("Unknown target col")
-    
     if len(target) == 0:
         # Happens sometimes with fp_prec / fn_rec
         return None
     
     # Obtain cluster labels
-    if method in ['kmeans', 'kmeans_cv']:
-        cluster_model = KMeans(n_clusters=5)
-        if method == 'kmeans':
-            cluster_model.fit(X_train)
-            labels = cluster_model.predict(X)
-        else:
-            labels = cluster_model.fit_predict(X)
-    elif method == 'hbac':
-        hbac = BiasAwareHierarchicalKMeans(n_iter=10, min_cluster_size=5)
+    if method == 'hbac':
+        hbac = BiasAwareHierarchicalKMeansV2(n_iter=10, min_cluster_size=5)
         hbac.fit(X, target)
         labels = hbac.labels_
+        return X, target, labels    
     elif method == 'randomclusters':
         labels = np.random.choice(range(5), size=X.shape[0])
+        return X, target, labels
     else:
         raise ValueError(f"Not a known method ({method})")
-
-    return X, target, labels
-
+    
 
 def check_before_test(c0, c1, min_samples=5):
     """Determines whether to do significance testing
@@ -90,36 +70,6 @@ def check_before_test(c0, c1, min_samples=5):
         return True
 
     return False
-
-
-def compute_statistics(X, target, idx, bonf_correct, n_clust):
-    """Compute statistics for each cluster/feature."""
-    c1, c0 = target[idx], target[~idx]
-    _, p_clust = ttest_ind(c1, c0, equal_var=False)
-    
-    if bonf_correct:
-        p_clust = p_clust * n_clust
-
-    diff_clust = c1.mean() - c0.mean()
-    
-    p_feat, diff_feat = [], []
-    for ii in range(X.shape[1]):
-        X_ = X[:, ii]
-        c1, c0 = X_[idx], X_[~idx]
-
-        should_continue = check_before_test(c0, c1)
-        if should_continue:
-            continue
-        
-        _, p = ttest_ind(c1, c0)
-        if bonf_correct:
-            p = p * X.shape[1]
-
-        p_feat.append(p)
-        diff = c1.mean() - c0.mean()
-        diff_feat.append(diff)
-
-    return p_clust, diff_clust, p_feat, diff_feat
 
 
 def simulate_experiment(method, N, K, target_col, bonf_correct=False, iters=100):
@@ -157,7 +107,14 @@ def simulate_experiment(method, N, K, target_col, bonf_correct=False, iters=100)
                 continue
 
             # Compute cluster stats and save in container
-            p_clust, diff_clust, p_feat, diff_feat = compute_statistics(X, target, idx, bonf_correct, n_clust)
+            c1, c0 = target[idx], target[~idx]
+            _, p_clust = ttest_ind(c1, c0, equal_var=False)
+
+            if bonf_correct:
+                p_clust = p_clust * n_clust
+
+            diff_clust = c1.mean() - c0.mean()
+    
             results_clust['iter'].append(i)
             results_clust['cluster_nr'].append(l)
             results_clust['p_clust'].append(p_clust)
@@ -168,21 +125,9 @@ def simulate_experiment(method, N, K, target_col, bonf_correct=False, iters=100)
                 # Save params
                 results_clust[p_name_].append(p_)
 
-            # Save feature stats separately
-            for i_feat, (p_feat_, diff_feat_) in enumerate(zip(p_feat, diff_feat)):
-                results_feat['iter'].append(i)
-                results_feat['feat_nr'].append(i_feat)
-                results_feat['p_feat'].append(p_feat_)
-                results_feat['diff_feat'].append(diff_feat_)
-                results_feat['size_feat'].append(idx.sum())
-
-                for p_name, p_ in params_:
-                    results_feat[p_name].append(p_)
-
     results_clust = pd.DataFrame(results_clust)
-    results_feat = pd.DataFrame(results_feat)
-
-    return results_clust, results_feat
+    
+    return results_clust
 
 
 if __name__ == '__main__':
@@ -190,22 +135,17 @@ if __name__ == '__main__':
     from params import ITERS
     from params import params
 
-    results_clust, results_feat = [], []
+    results_clust = []
     for params in tqdm(params):
 
         method, n, k, target_col, bonf_correct = params
 
-        results_clust_, results_feat_ = simulate_experiment(
+        results_clust_ = simulate_experiment(
             # Run experiment with `method`
             method, n, k, target_col, bonf_correct=bonf_correct, iters=ITERS
         )
-
         results_clust.append(results_clust_)
-        results_feat.append(results_feat_)
 
     results_clust = pd.concat(results_clust, axis=0)
-    results_feat = pd.concat(results_feat, axis=0)
     f_out = Path(__file__).parent / 'results_clust.csv'
     results_clust.to_csv(f_out, index=False)
-    f_out = Path(__file__).parent / 'results_feat.csv'
-    results_feat.to_csv(f_out, index=False)
